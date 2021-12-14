e3f14b2fd579e834468a169e9f60a377c5aa824a
ericYager <eyager@amazon.com>
2021-12-14 16:28:13 -0600

    adding packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
 import Routes from './Routes';
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
